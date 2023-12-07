20f1f3065ec08a072d17a26c2061cb42440d135c
JanM <jan.madon@gmail.com>
2023-12-07 09:59:39 +0100

    availability on ubuntu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -2,62 +2,13 @@
 
 <template>
     <!-- <div class="min-h-screen bg-gray-100"> -->
-    <div class="h-screen bg-gray-50 flex w-full gap-3">
+    <div class="h-screen bg-gray-50 flex w-full gap-4">
         <Navigation class="border-solid border-2 border-gren-500" />
         <!-- Page Heading -->
-        <nav class=" flex justify-end border-solid border-2 border-red-800">
-                
-                <header class="bg-white shadow" v-if="$slots.header">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        <slot name="header" />
-                    </div>
-                </header>
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 b:red">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-
-                        </div>
-
-                        <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <div class="ms-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                                {{ $page.props.auth.user.name }}
-
-                                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')">
-                                            Profile
-                                        </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
         <!-- Page Content -->
-        <main>
-
-
-            
+        <main class="flex flex-col flex-1 px-4 overflow-hidden">
+{{ $slots.header }}
+            <NavTop $slots.header = 'a'/>
 
             <div class="flex-1 flex flex-col overflow-hidden">
                 <slot />
@@ -68,7 +19,7 @@
 
 <script setup>
 import Navigation from '@/Components/app/Navigation.vue';
-// import NavTop from '@/Components/app/NavTop.vue';
+import NavTop from '@/Components/app/NavTop.vue';
 // import UserSettingsDropdown from '@/Components/app/UserSettingsDropdown.vue'
 import { ref } from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
