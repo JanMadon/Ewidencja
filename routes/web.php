223ab51e49b04c3b0fbb3060584c51475cb03119
Jan <jan.madon@gmail.com>
2023-12-28 08:56:00 +0100

    improved layout

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,13 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return to_route('login');
+    // return Inertia::render('Welcome', [
+        // 'canLogin' => Route::has('login'),
+        // 'canRegister' => Route::has('register'),
+        // 'laravelVersion' => Application::VERSION,
+        // 'phpVersion' => PHP_VERSION,
+    // ]);
 });
 
 Route::controller(AdminController::class)
